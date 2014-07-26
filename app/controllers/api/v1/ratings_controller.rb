1f49f96baa1c76324cd1b6ba785bd650833b1512
Brian Broll <brian.broll@gmail.com>
2014-07-26 11:16:03 -0500

    Added permit finding and consumer creating (as needed)

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,6 +1,7 @@
-class API::V1::RatingsController < ApplicationController
+class API::V1::RatingsController < API::V1::FeedbackController
   before_action :set_rating, only: [:show, :edit, :update, :destroy]
   before_action :build_consumer, only: [:create]
+  before_action :set_permit, only: [:create]
 
   # GET /ratings
   # GET /ratings.json
@@ -29,11 +30,8 @@ class API::V1::RatingsController < ApplicationController
     #TODO
     @rating = Rating.new(rating_params)
 
-    #Update the average rating/total ratings of corresponding permit
-    
-    #@consumer = Consumer.where(:uuid => @rating.consumer_id).first
-
     @rating.consumer = @consumer
+    @rating.permit = @permit
 
     # Create the ride if provided
     #if not ride_params[:start_latitude].nil?
@@ -44,19 +42,13 @@ class API::V1::RatingsController < ApplicationController
     #end
 
     # Check the amount of recent ratings 
-    if @rating.consumer.ratings.where(:created_at => 24.hours.ago..Time.now).to_a.length < @rating.trusted_app.max_daily_posts
-
-      # Adjust the given permit's meta data
-      @permit = @rating.permit 
-      @permit.average_rating = (@permit.average_rating * @permit.total_ratings + @rating.rating)/(@permit.total_ratings + 1)
-      @permit.total_ratings +=  1
-      @permit.save
-
-      # Link rating to ride
-      #@rating.ride_id = ride.nil? ? nil : ride.id
+    if @consumer.ratings.where(:created_at => 24.hours.ago..Time.now).to_a.length < @consumer.trusted_app.max_daily_posts
 
       respond_to do |format|
         if @rating.save
+          #Update the rating info
+          adjust_permit_average_rating
+
           format.json { render action: 'show', status: :created, location: @rating }
           #if not ride.nil?
             #ride.rating_id = @rating.id
@@ -68,19 +60,7 @@ class API::V1::RatingsController < ApplicationController
       end
     else
       respond_to do |format|
-        format.json { render json: { :error => "Too many recent requests" }, status: :unprocessable_entity }
-      end
-      #Add logger here
-      #TODO
-    end
- 
-    respond_to do |format|
-      if @rating.save
-        format.html { redirect_to @rating, notice: 'Rating was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @rating }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @rating.errors, status: :unprocessable_entity }
+        format.json { render json: { :error => "Too many recent requests" }, status: :locked }
       end
     end
   end
@@ -119,21 +99,36 @@ class API::V1::RatingsController < ApplicationController
       uuid = params[:consumer_id]
       @consumer = Consumer.where(:unique_user_id => uuid).first
 
-      if @consumer.nil?
+      if @consumer.nil? then
         #TODO Get the app signature hash!
         sha_hash = params[:app_signature]
 
-        @consumer = Consumer.new(:unique_user_id => @rating.consumer_id)
-        @consumer.trusted_app = TrustedApp.where(:sha_hash => sha_hash)
-        if @consumer.save then
-          format.json { render json: @consumer.errors, status: :unprocessable_entity } #Change this to a custom message TODO
+        trusted_app = TrustedApp.where(:sha_hash => sha_hash).first
+        @consumer = Consumer.new(:unique_user_id => uuid)
+        @consumer.trusted_app = trusted_app
+
+        if not @consumer.save then
+          @consumer = nil
         end
       end
 
+    end
+
+    def set_permit
+      beacon_id = params[:permit_beacon_id]
+      @permit = Permit.where(:beacon_id => beacon_id).first
+    end
+
+    def adjust_permit_average_rating
+      # Adjust the given permit's meta data
+      @permit.average_rating = (@permit.average_rating * @permit.total_ratings + @rating.rating)/(@permit.total_ratings + 1)
+      @permit.total_ratings +=  1
+      @permit.save
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rating_params
       params.require(:rating).permit(:rating, :comments)
     end
+
 end
