32d2e3ffc30b519fcdaef785b06e09842bfdfec4
Brian Broll <brian.broll@gmail.com>
2014-07-26 17:27:06 -0500

    Made sure consumer is selected based off uuid AND trusted app

--- app/controllers/api/v1/feedback_controller.rb
+++ app/controllers/api/v1/feedback_controller.rb
@@ -4,19 +4,24 @@ class API::V1::FeedbackController < ApplicationController
 
   protected
     def build_consumer
-      uuid = params[:consumer_id]
-      @consumer = Consumer.where(:unique_user_id => uuid).first
+      @consumer = nil
+      # Get the trusted app
+      sha_hash = params[:app_signature]
+      trusted_app = TrustedApp.where(:sha_hash => sha_hash).first
 
-      if @consumer.nil? then
-        #TODO Get the app signature hash!
-        sha_hash = params[:app_signature]
+      if not trusted_app.nil? then
+        uuid = params[:consumer_id]
+        @consumer = trusted_app.consumers.where(:unique_user_id => uuid).first
 
-        trusted_app = TrustedApp.where(:sha_hash => sha_hash).first
-        @consumer = Consumer.new(:unique_user_id => uuid)
-        @consumer.trusted_app = trusted_app
+        if @consumer.nil? then
+          #TODO Get the app signature hash!
 
-        if not @consumer.save then
-          @consumer = nil
+          @consumer = Consumer.new(:unique_user_id => uuid)
+          @consumer.trusted_app = trusted_app
+
+          if not @consumer.save then
+            @consumer = nil
+          end
         end
       end
 
