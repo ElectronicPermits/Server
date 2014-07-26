dec118a182c66077d51ed33c36cd8cad94e4e1b2
Brian Broll <brian.broll@gmail.com>
2014-07-26 17:21:25 -0500

    Added unique user id tests (presence, uniqueness)

--- test/models/consumer_test.rb
+++ test/models/consumer_test.rb
@@ -9,4 +9,19 @@ class ConsumerTest < ActiveSupport::TestCase
     consumer.trusted_app = nil
     assert_not consumer.save
   end
+
+  test "should not save without unique user id" do
+    consumer = consumers(:consumer_2)
+    consumer.unique_user_id = nil
+    assert_not consumer.save
+  end
+
+  test "should not save without UNIQUE user id" do
+    consumerData = consumers(:consumer_2)
+    consumer = Consumer.new
+    consumer.unique_user_id = consumerData.unique_user_id 
+    consumer.trusted_app = consumerData.trusted_app 
+
+    assert_not consumer.save
+  end
 end
