bf5581fb7b7ba7f68c66888c64e56207adfec3f8
Brian Broll <brian.broll@gmail.com>
2014-07-29 07:52:43 -0500

    Added app permissions enum

---
+++ config/initializers/constants.rb
@@ -0,0 +1,6 @@
+#App roles permission types enum
+APP_PERMISSIONS = {}
+APP_PERMISSIONS[:read] = 'read'
+APP_PERMISSIONS[:modify] = 'modify'
+APP_PERMISSIONS[:delete] = 'delete'
+APP_PERMISSIONS[:manage] = 'manage'
