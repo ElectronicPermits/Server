96b443c2ffa3bf0c08b16880ebe8dd67b8938588
Brian Broll <brian.broll@gmail.com>
2014-11-03 20:59:02 -0600

    Set up many-to-many between users and user perms

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,5 @@
 class Permission < ActiveRecord::Base
+  #Permissions for managing permits of the given type
   PERMISSION_TYPES = [ MANAGE = 'manage', CREATE = 'create', MODIFY = 'modify', DELETE = 'delete', RATE = 'rate', RECORD_SERVICE  = 'record_service' ]
   belongs_to :service_type
   has_and_belongs_to_many :trusted_apps
--- app/models/user_permission.rb
+++ app/models/user_permission.rb
@@ -2,7 +2,7 @@ class UserPermission < ActiveRecord::Base
   ACTION_TYPES = [ ALL = 'all', CREATE = 'create', MODIFY = 'modify', DELETE = 'delete' ]
   TARGET_TYPES = [ USERS = 'users', TRUSTED_APPs = 'trusted_apps' ]
 
-  belongs_to :user
+  has_and_belongs_to_many :user
 
   validates :action, presence: true,
     inclusion: { in: ACTION_TYPES }
