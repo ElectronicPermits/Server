d6931306d927a4608a8165b5761a9b9691eaf7d1
Brian Broll <brian.broll@gmail.com>
2014-11-03 20:58:44 -0600

    Fixed seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#User Permissions
+super_permissions = []
+UserPermission::ACTION_TYPES.each do |action|
+  UserPermission::TARGET_TYPES.each do |target|
+    permission = UserPermission.create({ action: action, target: target })
+    if permission.action == UserPermission::ALL then
+      super_permissions.push(permission)
+    end
+  end
+end
+
+#Initial admin user
+admin = User.create({ email: "admin@admin.com", password: "password", 
+              password_confirmation: "password" })
+
+super_permissions.each do |perm|
+  admin.user_permissions << perm
+end
+
+#Trusted App for self
+
+#Default Service Type
+# Taxis
+#TODO
