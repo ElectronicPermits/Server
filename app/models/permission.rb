b27d59e92d346a39987331c117e2fb985428378a
Brian Broll <brian.broll@vanderbilt.edu>
2014-11-28 08:43:54 -0600

    Updated to rails 4.1 enums

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,10 +1,9 @@
 class Permission < ActiveRecord::Base
   #Permissions for managing permits of the given type
-  PERMISSION_TYPES = [ MANAGE = 'manage', CREATE = 'create', MODIFY = 'modify', DELETE = 'delete', RATE = 'rate', RECORD_SERVICE  = 'record_service' ]
+  enum permission_type: [ :RATE, :RECORD_SERVICE ]
   belongs_to :service_type
   has_and_belongs_to_many :trusted_apps
 
   validates :service_type, presence: true
-  validates :permission_type, presence: true, 
-    inclusion: { in: PERMISSION_TYPES }
+  validates :permission_type, presence: true
 end
--- app/models/user_permission.rb
+++ app/models/user_permission.rb
@@ -1,11 +1,11 @@
 class UserPermission < ActiveRecord::Base
-  ACTION_TYPES = [ ALL = 'all', CREATE = 'create', MODIFY = 'modify', DELETE = 'delete' ]
-  TARGET_TYPES = [ USERS = 'users', TRUSTED_APPs = 'trusted_apps' ]
+  #ACTION_TYPES = Hash[ 'ALL', 'all', 'CREATE', 'create', 'MODIFY', 'modify', 'DELETE', 'delete' ]
+  enum action: [ :ALL, :CREATE, :MODIFY, :DELETE ]
+  enum target: [ :USERS, :TRUSTED_APPS ]
+  #TARGET_TYPES = Hash[ 'USERS', 'users', 'TRUSTED_APPs', 'trusted_apps' ]
 
   has_and_belongs_to_many :user
 
-  validates :action, presence: true,
-    inclusion: { in: ACTION_TYPES }
-  validates :target, presence: true, 
-    inclusion: { in: TARGET_TYPES }
+  validates :action, presence: true
+  validates :target, presence: true
 end
