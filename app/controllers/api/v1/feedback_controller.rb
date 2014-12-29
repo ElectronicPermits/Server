b89f80b5682b873a91dde104f79201862460be2a
Brian Broll <brian.broll@vanderbilt.edu>
2014-12-29 09:49:48 -0600

    Fixed double render error

--- app/controllers/api/v1/feedback_controller.rb
+++ app/controllers/api/v1/feedback_controller.rb
@@ -1,13 +1,13 @@
 # Base functionality for providing rating or service feedback to 
 # permits
 class API::V1::FeedbackController < API::V1::BaseController
+  #before_action :set_current_app, only: [:build_consumer]
 
   protected
     def build_consumer
-      set_current_app
       @consumer = nil
+      set_current_app
 
-      #@current_app = TrustedApp.where(:sha_hash => params[:app_signature]).first
       if not @current_app.nil? then
         uuid = params[:consumer_id]
         @consumer = @current_app.consumers.where(:unique_user_id => uuid).first
@@ -21,14 +21,13 @@ class API::V1::FeedbackController < API::V1::BaseController
             @consumer = nil
           end
         end
-      end
 
-      if @consumer.nil? then
-        respond_to do |format|
-        format.json { render json: { :error => "Application lacks permissions" }, status: :forbidden }
+        if @consumer.nil? then
+            respond_to do |format|
+                format.json { render json: { :error => "Application lacks permissions" }, status: :forbidden }
+            end
         end
       end
-
     end
 
 end
